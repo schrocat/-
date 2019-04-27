5451fc3d1ec3292efdbd7c1ecde49442eb2f39f5
schrocat <15hyxue@stu.edu.cn>
2019-04-27 22:47:35 +0800

    delete

--- config/config.prod.js
+++ config/config.prod.js
@@ -9,13 +9,6 @@ module.exports = appInfo => {
    */
   config.mysql = {
     clients: {
-        db1: {
-          host: '120.79.0.196',
-          port: '3306',
-          user: 'root',
-          password: 'sixue',
-          database: 'alumus',
-        },
         db2: {
           host: '182.254.231.154',
           port: '3306',
